04aeecdfd939d0fe29de16868dba14254f6c557b
Leo Peng <pengzimaobuaa@163.com>
2016-06-07 23:27:50 +0800

    Finish first feature

--- Fonts/RootViewController.h
+++ Fonts/RootViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  RootViewController.h
 //  Fonts
 //
 //  Created by Leo Peng on 6/7/16.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface RootViewController : UITableViewController
 
 @end
-
