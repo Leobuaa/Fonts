b4a2eecd1961df698eb5fdaf32e61ad1aa0b0ef2
Leo Peng <pengzimaobuaa@163.com>
2016-06-11 20:51:48 +0800

    add swipe delete feature

--- Fonts/FontListViewController.m
+++ Fonts/FontListViewController.m
@@ -66,25 +66,24 @@
 }
 
 
-/*
 // Override to support conditional editing of the table view.
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
     // Return NO if you do not want the specified item to be editable.
-    return YES;
+    return self.showsFavorites;
 }
-*/
 
-/*
 // Override to support editing the table view.
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (!self.showsFavorites) return;
+    
     if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
+        NSString *favorite = self.fontNames[indexPath.row];
+        [[FavoritesList sharedFavoritesList] removeFavorite:favorite];
+        self.fontNames = [FavoritesList sharedFavoritesList].favorites;
+        
         [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
+    }
 }
-*/
 
 /*
 // Override to support rearranging the table view.
